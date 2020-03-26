1eb2b5d3095666851079f137dbf3e8d8d108cd67
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-03-26 11:36:32 -0500

    all models built out

---
+++ Models/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DogWalkerAPI.Models
+
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int OwnerId { get; set; }
+        public String Breed { get; set; }
+        public string Notes { get; set; }
+        public OWNER Owner { get; set; }
+        public void walk(Walker walker, OWNER Owner, DateTime date, int duration)
+        {
+            Console.WriteLine($"{walker.Name} took on a {duration} minute walk.");
+        }
+    }
+}
