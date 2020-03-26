3af6b44470e904326b0af58ea02fcd9f6aaf7576
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-03-26 14:20:58 -0500

    Get all and get by ID on dog controller. When getting by id you get the full neighborhood info of their owner

---
+++ Controllers/DogController.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using DogWalkerAPI.Models;
+
+namespace DogWalkerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public DogController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // ----------Get all----------
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT d.Id, d.[Name], d.Breed, d.Notes, d.OwnerId, o.Id, o.[Name] AS OwnerName, o.NeighborhoodId
+                        FROM Dog d
+                        LEFT JOIN OWNER o
+                        ON d.OwnerId = o.Id";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Dog> allDogs = new List<Dog>();
+
+                    while (reader.Read())
+                    {
+                        var dog = new Dog()
+                        {
+
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                            OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                            Owner = new Owner()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                Name = reader.GetString(reader.GetOrdinal("OwnerName")),
+                                NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                //Neighborhood = new Neighborhood()
+                                //{
+
+                                //}
+
+                            }, 
+                            Notes = reader.GetString(reader.GetOrdinal("Notes"))
+                        };
+
+                        allDogs.Add(dog);
+                    }
+                    reader.Close();
+
+                    return Ok(allDogs);
+                }
+            }
+        }
+
+        //----------GET by Id----------
+        [HttpGet("{id}", Name = "GetDog")]
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT d.Id, d.[Name], d.Breed, d.Notes, d.OwnerId, o.Id, o.Phone, o.Address, o.[Name] AS OwnerName, o.NeighborhoodId, n.[Name] as NeighborhoodName, n.Id 
+                        FROM Dog d
+                        LEFT JOIN OWNER o
+                        ON d.OwnerId = o.Id
+                        LEFT JOIN Neighborhood n
+                        ON o.NeighborhoodId = n.Id
+                        WHERE d.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Dog dog = null;
+
+                    if (reader.Read())
+                    {
+                        dog = new Dog()
+                        {
+
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Breed = reader.GetString(reader.GetOrdinal("Breed")),
+                            OwnerId = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                            Owner = new Owner()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("OwnerId")),
+                                Name = reader.GetString(reader.GetOrdinal("OwnerName")),
+                                Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                                Address = reader.GetString(reader.GetOrdinal("Address")),
+                                NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                Neighborhood = new Neighborhood()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("NeighborhoodId")),
+                                    Name = reader.GetString(reader.GetOrdinal("NeighborhoodName")),
+
+                                }
+
+                            },
+                            Notes = reader.GetString(reader.GetOrdinal("Notes"))
+                        };
+                        reader.Close();
+
+                        return Ok(dog);
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+        }
+
+        //////----------POST----------
+
+        //[HttpPost]
+        //public async Task<IActionResult> Post([FromBody] Dog dog)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                INSERT INTO Dog (Name, OwnerId)
+        //                OUTPUT INSERTED.Id
+        //                VALUES (@Name,  @OwnerId)";
+        //            cmd.Parameters.Add(new SqlParameter("@Name", dog.Name));
+        //            cmd.Parameters.Add(new SqlParameter("@OwnerId", dog.OwnerId));
+
+        //            int id = (int)cmd.ExecuteScalar();
+
+        //            dog.Id = id;
+        //            return CreatedAtRoute("GetDog", new { id = id }, dog);
+        //        }
+        //    }
+        //}
+
+        //////----------PUT----------
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Dog dog)
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection conn = Connection)
+        //        {
+        //            conn.Open();
+        //            using (SqlCommand cmd = conn.CreateCommand())
+        //            {
+        //                cmd.CommandText = @"UPDATE Dog
+        //                             SET Name = @name, OwnerId = @neighborhoodId
+        //                             WHERE Id = @id";
+
+        //                cmd.Parameters.Add(new SqlParameter("@Name", dog.Name));
+        //                cmd.Parameters.Add(new SqlParameter("@neighborhoodId", dog.OwnerId));
+        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //                int rowsAffected = cmd.ExecuteNonQuery();
+        //                if (rowsAffected > 0)
+        //                {
+        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                }
+        //                throw new Exception("No rows affected");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        if (!DogExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
+
+        //////----------DELETE----------
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> Delete([FromRoute] int id)
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection conn = Connection)
+        //        {
+        //            conn.Open();
+        //            using (SqlCommand cmd = conn.CreateCommand())
+        //            {
+        //                cmd.CommandText = "DELETE FROM Dog WHERE Id = @id";
+        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //                int rowsAffected = cmd.ExecuteNonQuery();
+        //                if (rowsAffected > 0)
+        //                {
+        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                }
+        //                throw new Exception("No rows affected");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        if (!DogExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
+
+        //private bool DogExists(int id)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                SELECT Id, Name, OwnerId
+        //                FROM Dog
+        //                WHERE Id = @id";
+        //            cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //            SqlDataReader reader = cmd.ExecuteReader();
+        //            return reader.Read();
+        //        }
+        //    }
+        //}
+    }
+}
