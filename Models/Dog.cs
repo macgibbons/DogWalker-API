a16ba36aedf525afee6e23588bd54617737b1671
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-03-31 11:08:41 -0500

    added required data annotations on Walker Model

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DogWalkerAPI.Models
@@ -8,7 +9,9 @@ namespace DogWalkerAPI.Models
     public class Dog
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int OwnerId { get; set; }
         public string Breed { get; set; }
         public string Notes { get; set; }
